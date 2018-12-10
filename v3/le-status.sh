01de9e63dec94ecd9c4efeca957f0ea97c915e7b
Micro-Ram <webmaster@micro-ram.com>
2018-12-10 05:08:55 +0000

    le-status description

--- v3/le-status.sh
+++ v3/le-status.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+## Outputs a list of web sites and their LetsEncrypt expiration date
+## Example ./le-status.sh
+## Output
+## 20190101: example.com
+
+## Can be sorted by date
+## Example2 ./le-status.sh | sort
+
 for dir in /etc/letsencrypt/live/*/
 do
     dir=${dir%*/}
